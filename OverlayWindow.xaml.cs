c728bdd49e21ec347abc9dab84619d61db634b1f
shihwei.huang <shihwei.huang@gigabyte.com>
2026-03-19 17:42:06 +0800

    Feat: 實作 OverlayWindow 與滑鼠穿透 (Click-through) 功能

---
+++ OverlayWindow.xaml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace TransApp;
+
+public partial class OverlayWindow : Window
+{
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_TRANSPARENT = 0x00000020;
+    private const int WS_EX_LAYERED = 0x00080000;
+
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    public OverlayWindow()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        // 設置視窗為滑鼠穿透
+        var hwnd = new WindowInteropHelper(this).Handle;
+        int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+        SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
+    }
+
+    public void UpdateText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            this.Hide();
+        }
+        else
+        {
+            this.Show();
+            TranslatedText.Text = text;
+        }
+    }
+}
