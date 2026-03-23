572c2037ad969aea3c7fe7406ce4e286f5d2c39a
shihwei.huang <shihwei.huang@gigabyte.com>
2026-03-23 15:24:53 +0800

    Refactor: 移除自動捲動功能，簡化文字顯示邏輯

--- OverlayWindow.xaml.cs
+++ OverlayWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 
 namespace TransApp;
 
@@ -20,8 +19,6 @@ public partial class OverlayWindow : Window
     [DllImport("user32.dll")]
     private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-    private Storyboard? _scrollStoryboard;
-
     public OverlayWindow()
     {
         InitializeComponent();
@@ -30,8 +27,6 @@ public partial class OverlayWindow : Window
         this.Top = SystemParameters.VirtualScreenTop;
         this.Width = SystemParameters.VirtualScreenWidth;
         this.Height = SystemParameters.VirtualScreenHeight;
-
-        TranslatedText.RenderTransform = new TranslateTransform();
 
         ApplySettings();
     }
@@ -70,7 +65,6 @@ public partial class OverlayWindow : Window
         if (string.IsNullOrEmpty(text))
         {
             TranslationContainer.Visibility = Visibility.Collapsed;
-            StopScrolling();
         }
         else
         {
@@ -93,7 +87,6 @@ public partial class OverlayWindow : Window
 
     private void ApplyResponsiveLayout(Rect sourceArea, Rect targetRect)
     {
-        StopScrolling();
         var config = ConfigService.Current;
         double currentFontSize = config.FontSize;
         
@@ -118,8 +111,7 @@ public partial class OverlayWindow : Window
 
             if (isAbove)
             {
-                // 在上方時：計算剩餘向上空間
-                double spaceAbove = targetRect.Bottom - 5; // 距離頂部的距離
+                double spaceAbove = targetRect.Bottom - 5;
                 double maxAllowedHeight = Math.Min(desiredHeight, spaceAbove);
                 
                 double deltaHeight = maxAllowedHeight - targetRect.Height;
@@ -128,7 +120,6 @@ public partial class OverlayWindow : Window
             }
             else
             {
-                // 在下方時：計算剩餘向下空間
                 double screenBottom = this.Height;
                 double currentTop = targetRect.Y - this.Top;
                 double spaceBelow = screenBottom - currentTop - 5;
@@ -136,49 +127,6 @@ public partial class OverlayWindow : Window
 
                 TranslationContainer.Height = maxAllowedHeight;
             }
-
-            // 如果達到最大允許高度後還是放不下，就啟動捲動
-            CheckAndStartScrolling(TranslationContainer.Height);
-        }
-        else
-        {
-            CheckAndStartScrolling(TranslationContainer.Height);
-        }
-    }
-
-    private void CheckAndStartScrolling(double containerHeight)
-    {
-        StopScrolling();
-
-        double contentHeight = TranslatedText.ActualHeight;
-        double viewableHeight = containerHeight - 6;
-
-        if (contentHeight > viewableHeight)
-        {
-            double scrollDistance = contentHeight - viewableHeight;
-            var animation = new DoubleAnimation
-            {
-                From = 0,
-                To = -scrollDistance - 20,
-                Duration = TimeSpan.FromSeconds(Math.Max(3, scrollDistance / 20)),
-                AutoReverse = true,
-                RepeatBehavior = RepeatBehavior.Forever
-            };
-
-            _scrollStoryboard = new Storyboard();
-            _scrollStoryboard.Children.Add(animation);
-            Storyboard.SetTarget(animation, TranslatedText);
-            Storyboard.SetTargetProperty(animation, new PropertyPath("RenderTransform.(TranslateTransform.Y)"));
-            _scrollStoryboard.Begin();
-        }
-    }
-
-    private void StopScrolling()
-    {
-        _scrollStoryboard?.Stop();
-        if (TranslatedText.RenderTransform is TranslateTransform tt)
-        {
-            tt.Y = 0;
         }
     }
 }
